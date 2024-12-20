e9c04032105a6069d68784099dd24bfa8b00baa8
ameen <ptameen7@gmail.com>
2024-12-20 11:33:21 +0530

    'first'

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const btnClicked=(e)=>{
+    console.log(e.target.innerText)
+    console.log("hello all")
+    inp.value+=e.target.innerText
+}
+
+const getResult=()=>{
+    try{
+        const result=eval(inp.value)
+        inp.value=result
+    
+    }
+    catch{
+        inp.value="invalid Expression!!"
+    }
+}
+
+const backSpace=()=>{
+    inp.value=inp.value.slice(0,(inp.value.length-1))
+}
+
+const clearInp=()=>{
+    inp.value=""
+}
